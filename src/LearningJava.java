79ee68a34bd48cc9fcb3d4443761dea1c045f3b2
odalis <odalisgarcia502@gmail.com>
2023-02-14 20:27:21 -0500

    first commit

---
+++ src/LearningJava.java
@@ -0,0 +1,22 @@
+
+public class LearningJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Odalis G";
+		System.out.println(name.toUpperCase());
+		
+		addExclamationPoint("Programming in Java");
+		
+	}
+	
+	public static void  addExclamationPoint(String o) {
+		System.out.println(o + "!");
+	
+		
+		
+
+	}
+
+}
